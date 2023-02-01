471a2acc084a35300b78baa55310bdb51b056ff5
Toffi <59663047+Kartoffi@users.noreply.github.com>
2023-02-01 17:06:14 +0100

    Update ToDosView.vue

--- src/views/ToDosView.vue
+++ src/views/ToDosView.vue
@@ -26,8 +26,8 @@
       <input type="text" v-model="assignment" placeholder="New Assignment" class="input-task">
       <button @click="add" class="add-task"> +</button>
     </div>
-    <button @click="show = !show" v-if="show">Hide List</button>
-    <button @click="show = !show" v-else>Show List</button>
+    <button @click="show = !show" v-if="show" class="show-list">Hide List</button>
+    <button @click="show = !show" v-else class="show-list">Show List</button>
     <!-- </form> -->
 
     <ul v-if="show && list.length">
@@ -44,39 +44,57 @@
 </template>
 
 <style scoped>
-.input-task {
-  width: 100%;
-  padding: 0 10px;
-  height: 36px;
-  border: none;
-  border-radius: 3px 0px 0px 3px;
-  background-color: rgb(241, 241, 241);
-}
+  .input-task {
+    width: 100%;
+    padding: 0 10px;
+    height: 36px;
+    border: none;
+    border-radius: 3px 0px 0px 3px;
+    background-color: rgb(241, 241, 241);
+  }
 
-.add-task {
-  height: 36px;
-  width: 36px;
-  font-size: 24px;
-  font-weight: bold;
-  background-color: #509b00;
-  color: white;
-  border: none;
-  border-radius: 0px 3px 3px 0px;
-  cursor: pointer;
-}
+  .add-task {
+    height: 36px;
+    width: 36px;
+    font-size: 24px;
+    font-weight: bold;
+    background-color: #509b00;
+    color: white;
+    border: none;
+    border-radius: 0px 3px 3px 0px;
+    cursor: pointer;
+  }
 
-.add-task:hover {
-  background-color: #64c200;
-}
+  .add-task:hover {
+    background-color: #64c200;
+  }
 
  div {
-  display: flex;
-  width: 100%;
+    display: flex;
+    width: 100%;
+ }
+
+ .show-list {
+    background-color: #509b00;
+    color: white;
+    font-weight: bold;
+    border:none;
+    padding: 10px;
+    border-radius: 3px;
+    margin: 1rem 0;
+    cursor: pointer;
+    width: 90px;
+ }
+
+ .show-list:hover {
+    background-color: #64c200;
  }
 
   ul {
     padding-left: 0;
+    margin: 0;
   }
+
   li {
     display: flex;
     justify-content: space-between;
@@ -85,11 +103,20 @@
     padding: 1rem;
     border: 1px solid #75E200;
     margin: 0.5rem 0rem;
+  }
+
+  li:first-of-type{
+    margin-top: 0;
+  }
+
+  li, li * {
     cursor: pointer;
   }
+
   label {
     max-width: fit-content;
   }
+
   .check-box {
     min-width: 18px;
     height: 18px;
