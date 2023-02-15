5bf6270d66fb94009fea1c949b29c645b4d67463
Toffi <59663047+Kartoffi@users.noreply.github.com>
2023-02-15 14:54:21 +0100

    Add Tag.vue

---
+++ src/components/ToDoList/Tag.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import { useListStore } from "../../store/ListStore.js";
+  let list = useListStore();
+
+  const { tag, completed, selectedTag } = defineProps(['tag', 'completed', 'selectedTag']);
+</script>
+<template>
+  <button class="tag-button"
+  v-if="!list.emptyTaskList(tag, completed)"
+  :class="selectedTag == tag ? 'tag-highlight' : ''"> {{ tag }} ({{ list.tasksLength(tag) }})</button>
+</template>
+
+<style scoped>
+   .tag-button {
+    background-color: #659235;
+    color: white;
+    font-size: 12px;
+    font-weight: bold;
+    border:none;
+    padding: 10px;
+    border-radius: 3px;
+    cursor: pointer;
+ }
+
+ .tag-highlight {
+  background-color: #88ba53;
+ }
+
+ .tag-button:hover {
+  background-color: #78a746;
+ }
+</style>
